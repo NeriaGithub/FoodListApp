b144650db9e72015bfed48a54968538a3c0bafe5
NeriaGithub <neria,jerafi@gmail.com>
2021-01-21 13:26:35 +0200

    Initial Commit

---
+++ FoodListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodListApp
+//
+//  Created by Neria Jerafi on 21/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
